27b3311783667d7e58ef12c6acf4a1c762ebfc59
Zac Messsinger <zmessinger@quantummetric.com>
2019-11-25 20:46:09 -0800

    Added Spacer component and react-native-elements'

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-export default SignupScreen = () => {
-    return <Text style={{ fontSize: 48}}>SignupScreen</Text>;
+export default SignupScreen = ({ navigation }) => {
+    return (
+        <> 
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Spacer>
+                <Input label="Email"/>
+                <Input label="Password"/>
+                <Button title="Sign up"/>
+            </Spacer>
+        </>
+    )
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-export default TrackListScreen = () => {
-    return <Text style={{ fontSize: 48}}>TrackListScreen</Text>;
+export default TrackListScreen = ({ navigation }) => {
+    return (
+        <>
+            <Text style={{ fontSize: 48}}>TrackListScreen</Text>
+            <Button
+                title="Go to track detail"
+                onPress={() => navigation.navigate('TrackDetail')}/>
+        </>
+    )
 };
 
 const styles = StyleSheet.create({});
