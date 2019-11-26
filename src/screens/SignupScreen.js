1c8e72cc0db05eb0fa25a7e3fe1e3b562d262471
Zac Messsinger <zmessinger@quantummetric.com>
2019-11-25 21:00:14 -0800

    Added Controlled Inputs to SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
+// React Fragment - Don't show a wrapping element when the component renders on the screen.
+
 export default SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <> 
+        <View style={styles.container}> 
             <Spacer>
                 <Text h3>Sign Up for Tracker</Text>
             </Spacer>
             <Spacer>
-                <Input label="Email"/>
-                <Input label="Password"/>
-                <Button title="Sign up"/>
+                <Input 
+                    label="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <Input 
+                    secureTextEntry
+                    label="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <Button 
+                    title="Sign up"
+                    
+                />
             </Spacer>
-        </>
+        </View>
     )
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        // Hide Header
+        header: null,
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 100
+    }
+});
