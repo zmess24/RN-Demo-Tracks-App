ee5270cee175f140620010e1d57db41cc15ca5e5
Zac Messsinger <zmessinger@quantummetric.com>
2019-11-26 20:36:25 -0800

    Added AuthContext Methods

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,32 @@ const authReducer = (state, action) => {
     }
 };
 
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // Make API Request to signup with email + password.
+
+        // If we signup, modify our state, and say that we are authenticated.
+
+        // If signup fails, we need to reflect an error message somewhere.
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password}) => {
+        // Try to signin.
+        // Handle success by updating state.
+        // Handle failure by showing an error message (somehow).
+    };
+};
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow signout!!
+    };
+};
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signup, signin, signout },
     { isSignedIn: false }
 );
