a48bb03311b705f4b7ef95277957b1ac36461a6a
Zac Messsinger <zmessinger@quantummetric.com>
2019-12-04 20:37:31 -0800

    Added NavigationEvents to clear Error Message

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,16 +3,32 @@ import createDataContext from './createDataContext';
 import trackerAPI  from '../api/tracker';
 import { navigate } from '../navigationRef';
 
+/**
+|--------------------------------------------------
+| Reducer
+|--------------------------------------------------
+*/
+
 const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload };
         case 'signin':
-            return { errorMessage: '', token: action.payload }
+            return { errorMessage: '', token: action.payload };
+        case 'clear_err_message':
+            return { ...state, errorMessage: '' }; 
         default: 
             return state;
     }
 };
+
+/**
+|--------------------------------------------------
+| Action Functions
+|--------------------------------------------------
+*/
+
+const clearErrorMessage = dispatch => () => dispatch({ type: 'clear_err_message' });
 
 const signup = dispatch => async ({ email, password }) => {
     try {
@@ -44,8 +60,14 @@ const signout = dispatch => {
     };
 };
 
+/**
+|--------------------------------------------------
+| Exports
+|--------------------------------------------------
+*/
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signup, signin, signout },
+    { signup, signin, signout, clearErrorMessage },
     { token: null, errorMessage: '' }
 );
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 import { Context as AuthContext } from '../context/AuthContext';
 
 export default SigninScreen = () => {
-    const { state, signin } = useContext(AuthContext);
+    const { state, signin, clearErrorMessage } = useContext(AuthContext);
     return (
         <View style={styles.container}>
+            <NavigationEvents 
+                onWillBlur={clearErrorMessage}
+            />
             <AuthForm
                 headerText="Log In"
                 errorMessage={state.errorMessage}
